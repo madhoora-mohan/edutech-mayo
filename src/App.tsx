960eb7d5a35c530e294d3f198712eab96a6ec6c3
madhoora-mohan-s <madhooramohans@gmail.com>
2023-03-25 05:52:16 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import CodeTest from "./pages/CodeTest";
+import LeaderBoard from "./components/LeaderBoard";
+import LiveSession from "./pages/LiveSession";
+import PastContest from "./pages/PastContest";
+import Profile from "./pages/Profile";
+import Programming from "./pages/Programming";
+import Reports from "./pages/Reports";
+import Login from "./pages/Login";
+import ChangePwd from "./pages/ChangePwd";
 
 function App() {
   return (
-    <div className="App">
-      <div className="text-4xl underline">NoSkill-cit</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/ChangePwd" element={<ChangePwd />} />
+      <Route path="/CodeTest" element={<CodeTest />} />
+      <Route path="/LeaderBoard" element={<LeaderBoard />} />
+      <Route path="/LiveSession" element={<LiveSession />} />
+      <Route path="/PastContest" element={<PastContest />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/Programming" element={<Programming />} />
+      <Route path="/Reports" element={<Reports />} />
+    </Routes>
   );
 }
 
