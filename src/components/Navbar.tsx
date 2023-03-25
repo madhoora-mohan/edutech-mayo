62b35363e03277cc42696f33b9d911c18671dc71
madhoora-mohan-s <madhooramohans@gmail.com>
2023-03-25 06:03:56 +0530

    added navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  let logout = () => {
+    console.log("logged out");
+  };
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+            <Link to="/Reports">Reports</Link>
+            <Link to="/Profile">Profile</Link>
+            <Link to="/LeaderBoard">LeaderBoard</Link>
+            <button onClick={logout}>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
