2b445ea14f91cb796db18c5dba11a76363e780d4
madhoora-mohan-s <madhooramohans@gmail.com>
2023-03-26 14:39:55 +0530

    login page

--- src/components/LandingNav.tsx
+++ src/components/LandingNav.tsx
@@ -1,15 +1,18 @@
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const LandingNav = () => {
   return (
     <>
       <div className="flex justify-between p-5 items-center bg-[#1F1F1E] outline outline-[1px] outline-[#373737] rounded-sm">
-        <a href="./">
+        <Link to="/">
           <img src={logo} alt="logo" className="w-36 " />
-        </a>
-        <button className="h-12 outline-none  text-black rounded-md px-8 font-normal bg-[#2084D0] hover:bg-white hover:text-black">
-          Login
-        </button>
+        </Link>
+        <Link to="/login">
+          <button className="h-12 outline-none  text-black rounded-md px-8 font-normal bg-[#2084D0] hover:bg-[#2089F9] hover:text-black">
+            Login
+          </button>
+        </Link>
       </div>
     </>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import LandingNav from "../components/LandingNav";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <LandingNav />
+      <div className="h-[78vh] flex flex-col items-start p-10 gap-6">
+        <form className="w-2/5 flex flex-col items-start justify-center gap-2 p-16 pb-10 outline outline-[#373737] outline-1">
+          <input
+            placeholder="Login Id"
+            className="w-full h-10 bg-white rounded-md pl-4"
+          ></input>
+          <input
+            placeholder="Password"
+            className="w-full h-10 bg-white rounded-md pl-4"
+          ></input>
+          <button className="h-10 outline-none mt-4 text-white font-semibold rounded-md px-6 bg-[#2084D0] hover:bg-[#2089F9]">
+            Login
+          </button>
+        </form>
+        <Link to="/ChangePwd">
+          <button className="h-10 rounded-md bg-red-700 p-2 px-4 font-semibold">
+            Forgot password?
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default Login;
