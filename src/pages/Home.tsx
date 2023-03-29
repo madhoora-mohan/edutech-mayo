f4c53000e24ff84c116eab01eff7b477d6158fe9
madhoora-mohan-s <madhooramohans@gmail.com>
2023-03-29 11:21:56 +0530

    padding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <div className="mt-4 mb-2 flex gap-2 w-[97.5vw]">
         <LeftNav />
-        <div className="outline outline-1 outline-neutral-700 w-full myClassName">
+        <div className="outline outline-1 p-4 outline-neutral-700 w-full myClassName">
           Home
         </div>
       </div>
